471dfa708a59771f92227f54308bca38b722c0f4
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-03 20:49:59 +0300

    Initial Commit

---
+++ watch_timer WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watch_timer WatchKit Extension
+//
+//  Created by Nickolay Truhin on 03.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watch_timer WatchKit Extension/watch_timerApp.swift
@@ -0,0 +1,19 @@
+//
+//  watch_timerApp.swift
+//  watch_timer WatchKit Extension
+//
+//  Created by Nickolay Truhin on 03.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct watch_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
